6047d849407c98b2a70816c512730368dec30511
AbhiGTech <126134686+AbhiGTech@users.noreply.github.com>
2023-02-23 10:22:49 +0530

    Update streamlit_app.py
    added import for pandas
    added line to import csv into pandas dataframe
    added line to display pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('🍯 Kale Spinach and Rocket Smoothie')
 streamlit.text('🥚 Hard Boiled Free Range Eggs')
 streamlit.text('🍞 Avocado Toast')
 streamlit.title('🍌🍍🥭 Build your own smoothie 🍯')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
