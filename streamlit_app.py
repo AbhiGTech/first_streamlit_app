4ee565cf7180b2bf4cc0b083bbe4f8c27734e332
AbhiGTech <126134686+AbhiGTech@users.noreply.github.com>
2023-02-23 21:14:50 +0530

    Update streamlit_app.py
    removed line to output json return value

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # normalize(or remove the top level brackets) 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
